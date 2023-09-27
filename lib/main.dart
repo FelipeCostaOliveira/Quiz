b8bc00bbc023c844807684004ff559990a9e4137
felipe <felipeco144@gmail.com>
2023-09-27 17:46:23 -0400

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.black,
+          title: const Text('Quiz'),
+          iconTheme: const IconThemeData(color: Colors.white),
+        ),
+        body: const Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              FlutterLogo(size: 300),
+              ElevatedButton(
+                  onPressed:null,
+                  child: Text('Jogar', style: TextStyle(fontSize: 50),)),
+      
+            ],
+          ),
+        ));
+  }
+}
